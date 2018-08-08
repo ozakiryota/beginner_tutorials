2a421f39445addabea5b82508f6a3d2062332e01
ozakiryota <37431972+ozakiryota@users.noreply.github.com>
2018-08-08 19:23:23 +0900

    Update ekf.cpp

--- src/ekf.cpp
+++ src/ekf.cpp
@@ -16,6 +16,16 @@
 #include <geometry_msgs/Quaternion.h>
 #include <tf/tf.h>
 
+struct IMUDATA{
+	float wx;
+	float wy;
+	float wz;
+	float ax;
+	float ay;
+	float az;
+};
+
+std::vector<IMUDATA> record;
 sensor_msgs::Imu imu;
 ros::Time current_time;
 ros::Time last_time;
@@ -28,8 +38,8 @@ Eigen::MatrixXf P(3, 3);
 // Eigen::MatrixXf A(3, 3);
 // Eigen::MatrixXf B(3, 3);
 
-geometry_msgs::Quaternion q;
 
+geometry_msgs::Quaternion q;
 const double LOOP_RATE = 100.1;
 
 void strapdown(void)
@@ -64,10 +74,48 @@ void strapdown(void)
 	P = (I - K*H)*P;
 }
 
+bool judge_stopping
+{
+	const float threshold = 0.001;
+	if(fabs(record[record.size()-1].wx - ave.wx)>threshold)	return false;
+	if(fabs(record[record.size()-1].wy - ave.wy)>threshold)	return false;
+	if(fabs(record[record.size()-1].wz - ave.wz)>threshold)	return false;
+	if(fabs(record[record.size()-1].ax - ave.ax)>threshold)	return false;
+	if(fabs(record[record.size()-1].ay - ave.ay)>threshold)	return false;
+	if(fabs(record[record.size()-1].az - ave.az)>threshold)	return false;
+	std::cout << "non move" << std::endl;
+	return true;
+}
+
 void callback_imu(const sensor_msgs::ImuConstPtr& msg)
 {
 	// std::cout << "imu_callback" << std::endl;
 	imu = *msg;
+
+	IMUDATA ave = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	for(size_t i=0;i<record.size();i++){
+		ave.wx += record[i].wx/(float)record.size();
+		ave.wy += record[i].wy/(float)record.size();
+		ave.wz += record[i].wz/(float)record.size();
+		ave.ax += record[i].ax/(float)record.size();
+		ave.ay += record[i].ay/(float)record.size();
+		ave.az += record[i].az/(float)record.size();
+	}
+	record.push_back({
+		imu.angular_velocity.x,
+		imu.angular_velocity.y,
+		imu.angular_velocity.z,
+		imu.linear_acceleration.x,
+		imu.linear_acceleration.y,
+		imu.linear_acceleration.z
+	});
+	if(!first_callback){
+		if(judge_stopping)	strapdown();
+	}
+		
+	const size_t num_record = 100;
+	if(record.size()>num_record)	record.erace(record.begin());
+
 
 	current_time = ros::Time::now();
 	double dt = (current_time - last_time).toSec();
@@ -105,6 +153,8 @@ void callback_imu(const sensor_msgs::ImuConstPtr& msg)
 		X = A*X + B*U;
 		P = jF*P*jF.transpose();
 	}
+	
+	first_callback = false;
 }
 
 void callback_observation0(const geometry_msgs::QuaternionConstPtr& msg)
@@ -143,7 +193,7 @@ void callback_observation0(const geometry_msgs::QuaternionConstPtr& msg)
 	P = (I - K*H)*P;
 }
 
-void callback_observation(const sensor_msgs::PointCloud2ConstPtr& msg)
+void callback_observation_usingwalls(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
 	const float g = 9.80665;
 	Eigen::MatrixXf Z(2, 1);
@@ -152,7 +202,7 @@ void callback_observation(const sensor_msgs::PointCloud2ConstPtr& msg)
 	Eigen::MatrixXf H(2, 3);
 	H <<	1,	0,	0,
 			0,	1,	0;
-	Eigen::MatrixXf jH(2, 1);
+	Eigen::MatrixXf jH(2, 3);
 	jH <<	1,	0,	0,
 			0,	1,	0;
 	Eigen::MatrixXf I(3, 3);
@@ -164,6 +214,41 @@ void callback_observation(const sensor_msgs::PointCloud2ConstPtr& msg)
 	Eigen::MatrixXf Y(2, 1);
 	Eigen::MatrixXf S(2, 2);
 	Eigen::MatrixXf K(3, 2);
+	
+	Y = Z - H*X;
+	S = jH*P*jH.transpose();
+	K = P*H.transpose()*S.inverse();
+	X = X + K*Y;
+	P = (I - K*H)*P;
+}
+
+void callback_observation_slam(const geometry::QuoternionConstPtr& msg)
+{
+	float roll, pitch, yaw;
+	tf::Quaternion tmp_q(imu.orientation.x, imu.orientation.y, imu.orientation.z, imu.orientation.w);
+	tf::Matrix3x3(tmp_q).getRPY(roll, pitch, yaw);
+	
+	Eigen::MatrixXf Z(3, 1);
+	Z <<	roll,
+			pitch,
+			yaw;
+	Eigen::MatrixXf H(3, 3);
+	H <<	1,	0,	0,
+			0,	1,	0,
+			0,	0,	1;
+	Eigen::MatrixXf jH(3, 3);
+	jH <<	1,	0,	0,
+			0,	1,	0,
+			0,	0,	1;
+	Eigen::MatrixXf I(3, 3);
+	I <<	1,	0,	0,
+			0,	1,	0,
+			0,	0,	1;
+	Eigen::MatrixXf R(3, 3);
+	
+	Eigen::MatrixXf Y(3, 1);
+	Eigen::MatrixXf S(3, 3);
+	Eigen::MatrixXf K(3, 3);
 	
 	Y = Z - H*X;
 	S = jH*P*jH.transpose();
@@ -245,7 +330,7 @@ int main(int argc, char**argv)
 	last_time = ros::Time::now();
 
 	ros::Subscriber sub_imu = nh.subscribe("/imu/data", 10, callback_imu);
-	ros::Subscriber sub_obs = nh.subscribe("/g_from_normals", 10, callback_observation);
+	ros::Subscriber sub_obs = nh.subscribe("/g_usingwalls", 10, callback_observation_usingwalls);
 
 	Eigen::MatrixXf X = Eigen::MatrixXf::Zero(3, 1);
 	Eigen::MatrixXf P(3, 3);
